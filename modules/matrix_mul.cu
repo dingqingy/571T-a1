5adb8b47639295f39d2a2e6d62cc28b4ea63e652
Dingqing Yang <dingqingy@pleiades.ece.ubc.ca>
2019-04-21 21:53:49 -0700

    change data to double and add simple makefile

--- modules/matrix_mul.cu
+++ modules/matrix_mul.cu
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 // matrix multiplication: C = AB
 // A
-#define A_HEIGHT 35 
-#define A_WIDTH 30 
+#define A_HEIGHT 1024 
+#define A_WIDTH 1024 
 #define A_N A_HEIGHT * A_WIDTH 
 // B
 #define B_HEIGHT A_WIDTH
-#define B_WIDTH 35 
+#define B_WIDTH 1024 
 #define B_N B_HEIGHT * B_WIDTH
 
 // C
@@ -23,13 +24,13 @@
 #define BLOCK_SIZE 32
 #define MAX_ERR 1e-6
 
-__global__ void matrix_mul(float *d_C, float *d_A, float *d_B, int d_a_height, int d_a_width, int d_b_width) {
+__global__ void matrix_mul(double *d_C, double *d_A, double *d_B, int d_a_height, int d_a_width, int d_b_width) {
     int cid = blockIdx.y * blockDim.y + threadIdx.y;
     int rid = blockIdx.x * blockDim.x + threadIdx.x;
 
     if(rid < d_a_height && cid < d_b_width){
     // sum: to evaluated dot product
-        float sum = 0.0;
+        double sum = 0.0;
         for(int k = 0; k < d_a_width; k++){
             sum += d_A[rid * d_a_width + k] * d_B[d_b_width*k + cid];
         }
@@ -37,21 +38,21 @@ __global__ void matrix_mul(float *d_C, float *d_A, float *d_B, int d_a_height, i
     }
 }
 
-float float_rand( float min, float max )
-{
-    float scale = rand() / (float) RAND_MAX; 
-    return min + scale * ( max - min );      
-}
+// float float_rand( float min, float max )
+// {
+//     float scale = rand() / (float) RAND_MAX; 
+//     return min + scale * ( max - min );      
+// }
 
 int main(){
-    float *h_A, *h_B, *h_C;
-    float *d_A, *d_B, *d_C;
-    float *h_ref; // compute verified matMul
+    double *h_A, *h_B, *h_C;
+    double *d_A, *d_B, *d_C;
+    double *h_ref; // compute verified matMul
     // Allocate host memory
-    h_A = (float*)malloc(sizeof(float) * A_N);
-    h_B = (float*)malloc(sizeof(float) * B_N);
-    h_C = (float*)malloc(sizeof(float) * C_N);
-    h_ref = (float*)malloc(sizeof(float) * C_N);
+    h_A = (double*)malloc(sizeof(double) * A_N);
+    h_B = (double*)malloc(sizeof(double) * B_N);
+    h_C = (double*)malloc(sizeof(double) * C_N);
+    h_ref = (double*)malloc(sizeof(double) * C_N);
 
     // Initialize host arrays
     
@@ -67,22 +68,23 @@ int main(){
     //     }
     // }
     
+    srand((unsigned int)time(NULL));
     /***       TEST 2    ***/
     for (int i = 0; i< A_N; i++){
-        h_A[i] = float_rand(-1, 1);
+        h_A[i] = (double)rand()/(double)(RAND_MAX);
     }
     for (int i = 0; i< B_N; i++){
-        h_B[i] = float_rand(-1, 1);
+        h_B[i] = (double)rand()/(double)(RAND_MAX);
     }
 
     // Allocate device memory
-    cudaMalloc((void**)&d_A, sizeof(float) * A_N);
-    cudaMalloc((void**)&d_B, sizeof(float) * B_N);
-    cudaMalloc((void**)&d_C, sizeof(float) * C_N);
+    cudaMalloc((void**)&d_A, sizeof(double) * A_N);
+    cudaMalloc((void**)&d_B, sizeof(double) * B_N);
+    cudaMalloc((void**)&d_C, sizeof(double) * C_N);
 
     // Transfer data from host to device memory
-    cudaMemcpy(d_A, h_A, sizeof(float) * A_N, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, sizeof(float) * B_N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, h_A, sizeof(double) * A_N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, sizeof(double) * B_N, cudaMemcpyHostToDevice);
 
     // Executing kernel
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
@@ -93,12 +95,12 @@ int main(){
     matrix_mul<<<dimGrid,dimBlock>>>(d_C, d_A, d_B, A_HEIGHT, A_WIDTH, B_WIDTH);
     
     // Transfer data back to host memory
-    cudaMemcpy(h_C, d_C, sizeof(float) * C_N, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_C, d_C, sizeof(double) * C_N, cudaMemcpyDeviceToHost);
 
     // Verification
     for(int i = 0; i < C_HEIGHT; i++){
         for(int j = 0; j < C_WIDTH; j++){
-            float sum = 0.0;
+            double sum = 0.0;
             for(int k = 0; k < A_WIDTH; k++){
                 sum += h_A[i*A_WIDTH+k] * h_B[k*B_WIDTH + j];
             }
